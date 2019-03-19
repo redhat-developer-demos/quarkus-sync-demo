1664e8a8929b6907abcabac068c2d8d9c8b9aacd
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-03-19 17:53:25 +0530

    Repo init

---
+++ poll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url=$1
+if [ -z "$url" ]
+then
+    url=`minikube service quarkus-sync-demo --url`/hello
+fi
+
+while true
+do curl $url
+sleep 5
+done
---
+++ start-minikube.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+minikube profile quarkus-sync-demo
+
+minikube -p quarkus-sync-demo delete
+
+minikube start -p quarkus-app-demo --memory=4096 --cpus=2 
+
+# install ksync if not present locally 
+
+[[ ! -z command -v /usr/local/bin/ksync ]] && curl https://vapor-ware.github.io/gimme-that/gimme.sh | bash
+
+# ininitalize ksync
+ksync init 
+
+# configure bash to use minikube
+eval $(minikube docker-env)
