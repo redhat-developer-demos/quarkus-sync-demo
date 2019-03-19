007af39f2a694e3ad3baec2e66d49adfcb08bdc0
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-03-19 19:23:17 +0530

    (fix): script error evaluating ksync command
    Fixed the script error while evaluating `ksync` command exist

--- start-minikube.sh
+++ start-minikube.sh
@@ -6,11 +6,11 @@ minikube profile quarkus-sync-demo
 
 minikube -p quarkus-sync-demo delete
 
-minikube start -p quarkus-app-demo --memory=4096 --cpus=2 
+minikube start -p quarkus-sync-demo --memory=4096 --cpus=2 
 
 # install ksync if not present locally 
 
-[[ ! -z command -v /usr/local/bin/ksync ]] && curl https://vapor-ware.github.io/gimme-that/gimme.sh | bash
+[[ -z `command -v /usr/local/bin/ksync` ]] && curl https://vapor-ware.github.io/gimme-that/gimme.sh | bash
 
 # ininitalize ksync
 ksync init 
