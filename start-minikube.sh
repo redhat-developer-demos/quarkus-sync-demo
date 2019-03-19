30001e3ec6a65fc89ebf967cbe1d835806e09c1b
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-03-20 00:07:37 +0530

    (fix): add reload=false to avoid looping
    The ksync has an open issue https://github.com/vapor-ware/ksync/issues/180 when restarting the container, in this case since quarkus will reload the app on change we can disable the reload to avoid looping

--- start-minikube.sh
+++ start-minikube.sh
@@ -12,8 +12,12 @@ minikube start -p quarkus-sync-demo --memory=4096 --cpus=2
 
 [[ -z `command -v /usr/local/bin/ksync` ]] && curl https://vapor-ware.github.io/gimme-that/gimme.sh | bash
 
+# configure bash to use minikube
+eval $(minikube docker-env)
+
 # ininitalize ksync
 ksync init 
 
-# configure bash to use minikube
-eval $(minikube docker-env)
+# Wait for all inits to be done 
+sleep 10
+
